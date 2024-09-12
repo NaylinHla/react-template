f4c5083ca7fab3e90777c2750d6200ceb633007d
Naylin <naylinoohla@gmail.com>
2024-09-12 09:03:32 +0200

    Added CORS
    CORS (Cross-Origin Resource Sharing) is a security feature implemented in web browsers that controls how web pages can request resources from a different domain than the one that served the page.
    
     It's designed to prevent malicious websites from making unauthorized requests on behalf of a user to a different site.

--- server/api/Program.cs
+++ server/api/Program.cs
@@ -2,11 +2,22 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 builder.Services.AddOpenApiDocument();
+builder.Services.AddCors();
 
 var app = builder.Build();
 app.UseOpenApi();
 app.UseSwaggerUi();
 
 app.MapControllers();
+
+app.UseCors( opts => {
+
+    opts.AllowAnyOrigin();
+
+    opts.AllowAnyMethod();
+
+    opts.AllowAnyHeader();
+
+});
 
 app.Run();
--- server/api/obj/Debug/net8.0/api.AssemblyInfo.cs
+++ server/api/obj/Debug/net8.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+521bcba31f6627f4a1bf62e4eaaa0421a20a9ad1")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
